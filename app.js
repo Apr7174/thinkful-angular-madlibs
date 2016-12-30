276ff64091220ba514de9e685748f4edab105be5
Apr7174 <apr.7174@gmail.com>
2016-12-30 16:16:54 -0500

    added a controller

---
+++ app.js
@@ -0,0 +1,12 @@
+//creates a module
+angular.module('myApp', [])
+	.controller('MyController', function($scope){
+		$scope.name='person name';
+		$scope.jobTitle= 'job title';
+		$scope.tediousTask = 'tedious task';
+		$scope.dirtyTask = 'dirty task';
+		$scope.celebrity = 'celebrity';
+		$scope.celebrityTwo = 'another celebrity';
+		$scope.skill = 'skill';
+		$scope.number = 'number'
+	});
