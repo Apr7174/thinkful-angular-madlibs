e764a1314b1a0caa3359da4ea054388012431f47
Apr7174 <apr.7174@gmail.com>
2017-01-01 14:43:03 -0500

    added controller as vm, needs revision

--- app.js
+++ app.js
@@ -1,20 +1,37 @@
 //creates a module
 angular.module('myApp', [])
-	.controller('MyController', function($scope){
-		this.name='person name';
-		this.jobTitle= 'job title';
-		this.tediousTask = 'tedious task';
-		this.dirtyTask = 'dirty task';
-		this.celebrity = 'celebrity';
-		this.celebrityTwo = 'another celebrity';
-		this.skill = 'skill';
-		this.number = 'number'
+	.controller('MyController', function(){
+		// this.name='';
+		// this.jobTitle= '';
+		// this.tediousTask = 'tedious task';
+		// this.dirtyTask = 'dirty task';
+		// this.celebrity = 'celebrity';
+		// this.celebrityTwo = 'another celebrity';
+		// this.skill = 'skill';
+		// this.number = 'number'
 
 		this.gender = 'f';
 
-		$scope.changeGender = function() {
-			$scope.gender = 'male';
+		function changeGender() {
+			//this.gender = 'male';
+			console.log("changeGender");
 		}
 
-		this.showParagraph = true;
+		this.changeGender = changeGender;
+
+		this.showParagraph = false;
+
+		//didnt work - created a new "this" reference
+		//creates a functino called submitForm
+		// $scope.submitForm = function(){
+		// 	console.log("submitForm");
+		// 	this.showParagraph = !this.showParagraph;
+		// }
+
+		function submitForm(){
+			this.showParagraph = !this.showParagraph;
+		}
+
+		this.submitForm = submitForm;
+
 	});
